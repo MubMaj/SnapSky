4edcefe5cf5c8ec9db7cc61812765d775bcf2f80
MubMaj <mub.maj@gmail.com>
2024-02-27 11:03:39 +0530

    add geo location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, interval, } from 'rxjs';
 import { WeatherService } from '../service/weather.service';
 import { PhotoService } from '../service/photo.service';
 import { BackgroundService } from '../service/background.service';
@@ -27,6 +27,9 @@ export class HomeComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
+
+    this.getLocationAndFetchWeather();
+
     this.photoSubscription = interval(3000).subscribe(() => {
       if (this.PhotoData?.length) {
         const randomIndex = Math.floor(Math.random() * this.PhotoData.length);
@@ -40,13 +43,56 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.photoSubscription.unsubscribe();
   }
 
+  getLocationAndFetchWeather() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          const locationString = `${latitude},${longitude}`;
+
+          this.fetchWeatherData(locationString);
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }
+
+  fetchWeatherData(location: string) {
+
+    this.service.getWeatherData(location).subscribe(
+      (res) => {
+        this.weatherData = res;
+        console.log(this.weatherData);
+        const city = this.weatherData.location.name
+
+        this.service2.getPhotoData(city).subscribe((res) => {
+          this.PhotoData = res.photos || [];
+    
+          if (this.PhotoData?.length) {
+            const backgroundImage = `url("${this.PhotoData[0]?.src?.landscape}")`;
+            this.BgService.updateBackgroundImage(backgroundImage);
+          }
+        });    
+      },
+      (error) => {
+        console.error('Error fetching weather data:', error);
+      }
+    );
+
+    }
+
   searchWeather() {
     const city = this.searchForm.get('city')?.value;
 
     this.service.getWeatherData(city).subscribe(
       (res) => {
         this.weatherData = res;
-        console.log(this.weatherData);
         
       },
       (error) => {
