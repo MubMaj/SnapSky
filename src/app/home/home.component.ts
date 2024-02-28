5d6d9cc9c2226607e9e6fe65fe5803afaad99f2b
MubMaj <mub.maj@gmail.com>
2024-02-28 16:44:22 +0530

    error handle lite

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subscription, interval, } from 'rxjs';
+import { Subscription, interval } from 'rxjs';
 import { WeatherService } from '../service/weather.service';
 import { PhotoService } from '../service/photo.service';
 import { BackgroundService } from '../service/background.service';
@@ -17,6 +17,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   weatherData: any = { location: { name: "No Man's Land" }, current: { temp_c: '21' } };
   PhotoData: any[] = [{ src: { landscape: './assets/img/temp.jpg' } }];
+  errorMessage: string = '';
   private photoSubscription: Subscription = new Subscription();
 
   constructor(
@@ -27,7 +28,6 @@ export class HomeComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-
     this.getLocationAndFetchWeather();
 
     this.photoSubscription = interval(3000).subscribe(() => {
@@ -64,41 +64,33 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   fetchWeatherData(location: string) {
-
     this.service.getWeatherData(location).subscribe(
       (res) => {
-        this.weatherData = res;
-        console.log(this.weatherData);
-        const city = this.weatherData.location.name
-
-        this.service2.getPhotoData(city).subscribe((res) => {
-          this.PhotoData = res.photos || [];
-    
-          if (this.PhotoData?.length) {
-            const backgroundImage = `url("${this.PhotoData[0]?.src?.landscape}")`;
-            this.BgService.updateBackgroundImage(backgroundImage);
-          }
-        });    
+        this.handleWeatherDataResponse(res);
       },
       (error) => {
-        console.error('Error fetching weather data:', error);
+        this.handleWeatherDataError(error);
       }
     );
-
-    }
+  }
 
   searchWeather() {
     const city = this.searchForm.get('city')?.value;
+    this.errorMessage = '';
 
     this.service.getWeatherData(city).subscribe(
       (res) => {
-        this.weatherData = res;
-        
+        this.handleWeatherDataResponse(res);
       },
       (error) => {
-        console.error('Error fetching weather data:', error);
+        this.handleWeatherDataError(error);
       }
     );
+  }
+
+  private handleWeatherDataResponse(res: any) {
+    this.weatherData = res || {};
+    const city = this.weatherData.location.name;
 
     this.service2.getPhotoData(city).subscribe((res) => {
       this.PhotoData = res.photos || [];
@@ -108,5 +100,12 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.BgService.updateBackgroundImage(backgroundImage);
       }
     });
+  }
+
+  private handleWeatherDataError(error: any) {
+    let err = error.error.error.message as string;
+    this.errorMessage = err;
+    this.weatherData = { location: { name: "No Man's Land" }, current: { temp_c: '21' } };
+    this.PhotoData = [{ src: { landscape: './assets/img/temp.jpg' } }];
   }
 }
